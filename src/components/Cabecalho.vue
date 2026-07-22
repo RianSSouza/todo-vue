3bdb2ffbbac5c18eb805c305f3ac669adcc8ad48
Rian <rianssouza02@gmail.com>
2026-07-22 14:51:43 -0300

    Atualizando com o desafio

--- src/components/Cabecalho.vue
+++ src/components/Cabecalho.vue
@@ -4,6 +4,8 @@ const props = defineProps(['tarefasPendentes'])
 <template>
     <header class="p-5 mb--4 mt-4 bg-light rounded-3">
         <h1>Minhas tarefas</h1>
-        <p>Você possui {{ props.tarefasPendentes }} tarefas pendnetes</p>
+        <p v-if="props.tarefasPendentes > 0">Você possui {{ props.tarefasPendentes }} tarefas pendnetes</p>
+    
+        <p v-else class="fst-italic">Não há tarefas pendentes</p>
     </header>
 </template>
